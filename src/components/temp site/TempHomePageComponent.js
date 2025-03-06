d289f986f63d0a21405f7df241d0a3e9c666468c
jackie-shawley <jackiekopco@aol.com>
2025-03-06 11:53:22 -0500

    fixed error

--- src/components/temp site/TempHomePageComponent.js
+++ src/components/temp site/TempHomePageComponent.js
@@ -339,7 +339,7 @@ export default function TempHomePage() {
                             </Row>
                         </div> */}
                     </div>
-                </div>
+                
             </React.Fragment>
         )
 
